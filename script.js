7e36b52e4a48b5da7292c7fc3ff0646308a58c93
ParaskiMoveis <hubdeviagit@gmail.com>
2026-04-03 00:58:14 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const menuToggle = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+const links = document.querySelectorAll('.nav-links li a');
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        if(navLinks.classList.contains('active')) {
+            navLinks.classList.remove('active');
+        }
+    });
+});
